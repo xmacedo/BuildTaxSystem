4a433384dd742763106482dbdc32896f0385463d
Felipe Macedo <felipe.x.macedo@gmail.com>
2025-03-30 22:39:45 -0300

    WIP Build tax System, add method to calculate and create class to save data

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,4 +1,20 @@
-@main
-def main(): Unit = {
-  println("Hello world!")
+// Build a TAX system where different
+// products have
+// different tax per state and year. (on Scala)
+case class TaxRate(state: String, year: Int, rate: Double)
+
+object TaxSystem {
+  def calculateTotalPrice(basePrice: Double, state: String, year: Int): Double = {
+
+  }
+
+}
+object main extends App{
+  //println("Hell222o world!")
+  val price = 100.0
+  val state = "CA"
+  val year = 2024
+
+  println(s"Total price in $state for $year: $" + TaxSystem.calculateTotalPrice(price, state, year))
+
 }
